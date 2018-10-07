bee946023e2f68698ba9ba8fc02946ba13586c38
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2018-10-07 19:46:35 +0200

    reading graph from file

---
+++ PEA_TSP_1/Graph.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PEA_TSP_1
+{
+    public class Graph
+    {
+        private int[,] _graph;
+        private int _numberOfCities;
+
+        public Graph(int numberOfCities)
+        {
+            _numberOfCities = numberOfCities;
+            _graph = new int[numberOfCities - 1, numberOfCities - 1];
+        }
+
+        public Graph(string fileName)
+        {
+            try
+            {  
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string text = sr.ReadLine();
+
+                    _numberOfCities = Int32.Parse(text);
+                    _graph = new int[_numberOfCities, _numberOfCities];
+
+                    for (int i = 0; i < _numberOfCities; i++)
+                    {
+                        text = sr.ReadLine();
+                        var numbersAsText = text.Split(new string[]{"", " "}, StringSplitOptions.RemoveEmptyEntries);
+
+                        for (int j = 0; j < _numberOfCities; j++)
+                        {
+                            _graph[i, j] = Int32.Parse(numbersAsText[j]);         
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public int GetWeight(int x, int y)
+        {
+            return _graph[x, y];
+        }
+
+        public void Write()
+        {
+            for (int i = 0; i < _numberOfCities; i++)
+            {
+                for (int j = 0; j < _numberOfCities; j++)
+                {
+                    Console.Write(_graph[i,j] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
--- PEA_TSP_1/Program.cs
+++ PEA_TSP_1/Program.cs
@@ -6,7 +6,11 @@ namespace PEA_TSP_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var graph = new Graph("C:\\Users\\Piotr Borowski\\source\\repos\\PEA_TSP_1\\PEA_TSP_1\\data6.txt");
+
+            Console.WriteLine(graph.GetWeight(1, 0));
+            graph.Write();
+            Console.Read();
         }
     }
 }
